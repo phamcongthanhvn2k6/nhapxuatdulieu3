0d7caf1fae2cdc2f796efa72063f447c98dc4df3
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2024-12-10 20:56:05 +0700

    Add files via upload

---
+++ xuatnhapten.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+using namespace std; 
+	
+	int main(){
+		char ten[50];
+		
+		printf("nhap ten cua ban: ");
+		scanf("%s", ten);
+		
+		printf(" xin chao %s!\n", ten);
+		
+		return 0; 
+	} 
