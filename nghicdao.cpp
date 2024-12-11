266fbe5648096971ebff069f408e80e1dae02e7e
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2024-12-11 17:07:07 +0700

    Update nghicdao.cpp

--- nghicdao.cpp
+++ nghicdao.cpp
@@ -1,24 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>;
 
-int main() {
-    int so, nghichDao = 0;
-    
-    printf("Nhap so nguyen co 4 chu so: ");
-    scanf("%d", &so);
-    
-    if (so < 1000 || so > 9999) {
-        printf("Vui long nhap mot so co 4 chu so!\n");
-        return 1;
-    }
+using namespace std;
 
-    while (so != 0) {
-        int chuSo = so % 10;
-        nghichDao = nghichDao * 10 + chuSo;
-        so /= 10;
-    }
-
-    printf("So nghich dao la: %d\n", nghichDao);
-    
-    return 0;
-}
-
+	int main(){
+		int so, chuso, nghichdao;
+		printf("nhap chu so co 4 chu so: ");
+		scanf("%d",&so);
+		
+		if (so<1000 or so>9999){
+			printf("chu so khong hop le");
+			return 1;
+		}
+		else{
+			while (so != 0){
+				chuso = so%10;
+				nghichdao = nghichdao*10+chuso;
+				so /=10;
+			}
+			printf ("nghich dao cua chu su co 4 chu so la: %d",nghichdao);
+		}
+		return 0;
+		
+	}
